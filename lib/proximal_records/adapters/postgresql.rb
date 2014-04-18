eabb5fd17b56682826b8a519608d5d2300ac60ba
Dmitry Polushkin <dmitry.polushkin@gmail.com>
2014-04-18 13:53:52 +0100

    fix postgresql adapter code

--- lib/proximal_records/adapters/postgresql.rb
+++ lib/proximal_records/adapters/postgresql.rb
@@ -2,18 +2,19 @@ module ProximalRecords
   module Adapters
     module Postgresql
       def proximal_records(scope)
+        klass = self.class
+
         orders = scope.orders.join(', ')
 
         orders = "OVER(#{"ORDER BY #{orders}" if orders.present?})"
-        with_near_by = scope.select("articles.*, LAG(articles.id) #{orders} AS previous_id, LEAD(articles.id) #{orders} AS next_id")
+        primary_key = "#{klass.table_name}.#{klass.primary_key}"
+        with_near_by = scope.select("#{klass.table_name}.*, LAG(#{primary_key}) #{orders} AS previous_id, LEAD(#{primary_key}) #{orders} AS next_id")
 
         table = with_near_by.arel
-        as = table.as(Arel.sql('articles'))
-        a = self.class.from(as.to_sql).where(articles: {id: id}).limit(1)
+        as = table.as(Arel.sql('z'))
+        a = klass.select('z.*').from(as.to_sql).where(z: {klass.primary_key => id}).limit(1)[0]
 
-        a = a.first
-        previous_id, next_id = a.previous_id, a.next_id
-        [(self.class.find_by_id(previous_id)), (self.class.find_by_id(next_id))]
+        [(klass.find_by_id(a.previous_id)), (klass.find_by_id(a.next_id))]
       end
     end
   end
